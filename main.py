7afc4039994a7b934a1604ae610bf3c3c0b36f4d
Scripter357 <scripter357@gmail.com>
2021-12-24 18:39:31 +0300

    update

--- main.py
+++ main.py
@@ -12,22 +12,23 @@ print(x)
 @bot.message_handler(commands=['start'])
 def start(message):
     keyboard = types.ReplyKeyboardMarkup()
-    keyboard.row("Хочу", "/help")
+    keyboard.row("Хочу", "нет", "кто автор?", "/help")
     bot.send_message(message.chat.id, 'Привет! Хочешь узнать свежую информацию о МТУСИ?', reply_markup=keyboard)
 
 
 @bot.message_handler(commands=['help'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Я умею:\n- Тестовые команды: /whats2by2 /ignorethis /somethingtoread \n- Тестовые варианты: "хочу", "нет", "Кто автор?"')
+    bot.send_message(message.chat.id, 'Я умею:\n /whats2by2 - тест 2+2\n /randomsite - случайный сайт в Сети\n /somethingtoread - случайная страница википедии\n')
 
 
 @bot.message_handler(commands=['whats2by2'])
 def start_message(message):
     bot.send_message(message.chat.id, '4')
 
-@bot.message_handler(commands=['ignorethis'])
+@bot.message_handler(commands=['randomsite'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'OK')
+    bot.send_message(message.chat.id, 'Got it.')
+    bot.send_message(message.chat.id, "https://therandombutton.github.io/random.html")
 
 @bot.message_handler(commands=['somethingtoread'])
 def start_message(message):
@@ -41,14 +42,8 @@ def start_message(message):
 def answer(message):
     if message.text.lower() == "хочу":
         bot.send_message(message.chat.id, 'Тогда тебе сюда – https://mtuci.ru/')
-
-@bot.message_handler(content_types=['text'])
-def answer(message):
     if message.text.lower() == "нет":
         bot.send_message(message.chat.id, 'Ну ладно :/')
-
-@bot.message_handler(content_types=['text'])
-def answer(message):
     if message.text.lower() == "кто автор?":
         bot.send_message(message.chat.id, 'Павел из БВТ2108')
 
